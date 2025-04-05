a344e7e2cc945fd7a5c247e17bc759baa5c88d82
M260 <41881281+M2600@users.noreply.github.com>
2025-04-05 14:29:52 +0900

    wezterm open hyperlinks with Ctrl+Click

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,8 +6,31 @@ local config = {
 	font = wezterm.font("fira code"),
 	font_size = 14.0,
 	color_scheme = 'Catppuccin Mocha',
+	--color_scheme = 'Afterglow',
 	hide_tab_bar_if_only_one_tab = true,
 	audible_bell = "Disabled"
+}
+
+
+-- Set open link with Ctrl+Click
+config.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = 'Left'} },
+		mods = 'CTRL',
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+	-- Disable the 'Down' event 
+	{
+		event = { Down = { streak = 1, button = 'Left'} },
+		mods = 'CTRL',
+		action = wezterm.action.Nop,
+	},
+	-- Disable the 'Click' event
+	{
+		event = { Up = {streak = 1, button = 'Left'} },
+		mods = 'NONE',
+		action = wezterm.action.Nop,
+	},
 }
 
 
