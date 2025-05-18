6f764ec2f4311eb3e971f9bb4a246696731f1569
M260 <41881281+M2600@users.noreply.github.com>
2025-05-18 23:44:23 +0900

    Add sleep in panel_setting.sh\n\nAdd 10 sec of sleep for finishing the load of xfce4-panel

--- sh/panel_setting.sh
+++ sh/panel_setting.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Set Object named xfce4-panel to below with wmctrl.
+# Wait for 10 sec for finishing the load of xfce4-panel
+sleep 10
 
+
+# Set Object named xfce4-panel to below with wmctrl.
 while read line
 do
 	echo ">> $line"
