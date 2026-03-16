400528078ead9cd58751b86f16b79b6a8fe2f43d
M260 <41881281+M2600@users.noreply.github.com>
2026-03-17 01:54:36 +0900

    thinkpad trackpad

--- sh/reThinkpadTouchpad.sh
+++ sh/reThinkpadTouchpad.sh
@@ -3,3 +3,5 @@
 sudo modprobe -r elan_i2c psmouse i2c_i801
 sudo modprobe i2c_i801
 sudo modprobe psmouse elan_i2c
+
+sudo -u m260 libinput-gestures-setup restart >> /home/m260/tmp/log/libinput-gestures-setup.log 2>&1
